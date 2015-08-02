2ec11d387d136e1f1fc1916a6019ecf928142cc4
Hayden Parker <haydenkparker@gmail.com>
2015-08-01 22:53:13 -0700

    server

--- client.go
+++ client.go
@@ -16,18 +16,20 @@ type Client struct {
 	Writing		*sync.Mutex
 }
 
-type Request struct {
+//move to common
+type Request struct {		// maybe call it a query if it is goes to be used both ways?
 	RequestID	uint16
 	Type		uint16
 	Data		string
 }
+
 
 func NewClient(socket *net.Conn, types map[uint16]func(), type_codes map[reflect.Type]uint16) Client {
 	client := Client {
 		Socket:		socket,
 		Types:		types,
 		TypeCodes:	type_codes,
-		Requests:	make(map[reflect.Type][]func(interface{}))
+		Requests:	make(map[reflect.Type][]func(interface{})),
 		NextID:		1
 	}
     go process(client)
@@ -49,7 +51,7 @@ func process(client Client) {
 	// read from client's Socket and decide what to do with the struct
 	//read and parse header, read binary of struct
 	
-	// rather than a struct type and a size, how about a requestID and a size come back.  it is implicit that he stcut that comes back is of response type, a struct that contains the requiest ID, contained stuct type, and struct data.  format it.
+	// rather than a struct type and a size, how about a requestID and a size come back.  it is implicit that he stcut that comes back is of response type, a struct that contains the requiest ID, contained stuct type, and struct data.  format it.  this is the same as a request.
 	
 	// if it is a response to a previous request
 	//if msg.type == resp	// they are all going to be of type resp right?  at the outer layer yes.  outer layer is just requestID (and actual inner struct type)
@@ -77,9 +79,9 @@ func (client *Client) Message(instance Interface{}) error {
 
 func (client *Client) Request(instance Interface{}) Request {
 	request := Request {
-		RequestID:	client.getRequestID()
-		Type:		client.TypeCodes[Reflect.TypeOf(instance)]
-		Data:		json.Marshel(instance)
+		RequestID:	client.getRequestID(),
+		Type:		client.TypeCodes[Reflect.TypeOf(instance)],
+		Data:		json.Marshal(instance)
 	}
 	Message(request)
 	return request
--- server.go
+++ server.go
@@ -1,26 +1,47 @@
 package tlj
 
+import (
+	"fmt"
+	"reflect"
+	"encoding/json"
+	"encoding/binary"
+)
+
 type Server struct {
 	Listener	*net.UnixListener
 	Sockets		map[string][]*net.Conn							// sockets tagged with strings
 	Tag			func(*net.Conn)
 	Events		map[string]map[reflect.Type][]func(interface{})						//  string tag -> map from types to funcs
-	Requessts	map[string]map[reflect.Type][]func(*net.Conn, uint16, interface{})	//  string tag -> map from types to funcs
+	Requests	map[string]map[reflect.Type][]func(*net.Conn, uint16, interface{})	//  string tag -> map from types to funcs
 }
 
 func NewServer(listener *net.UnixListener, tag func(*net.Conn)) Server {
+	server := Server{
+		Listener:	listener,
+		Sockets:	make(map[string][]*net.Conn),
+		Tag:		tag,
+		Events:		make(map[string]map[reflect.Type][]func(interface{})),
+		Requests:	make(map[string]map[reflect.Type][]func(*net.Conn, uint16, interface{}))
+	}
 	
+	go server.process()
+	return server
 }
 
-func (server *Server) process(socket) {
+func (server *Server) process() {
 	// accept new connections from Listener
 	
-	// read from listener
+	// read from all sockets
+	// if request
+		// lookup funcs from Requests
+	// if anything else
+		// lookup func from Events
 }
 
 func (server *Server) Accept(socket_tag string, struct_type reflect.Type, function func(interface{})) {
+	// add to events map
 }
 
 func (server *Server) AcceptRequest(socket_tag string, struct_type reflect.Type, function func(*net.Conn, uint16, interface{})) {
-	// responses must go back the proper socket (also pass as a parameter?)
+	// add to requests map
 }
