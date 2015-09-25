c3cfd457abfdc5f8a1f3d253304fc914583dcfe9
Hayden Parker <haydenkparker@gmail.com>
2015-09-24 20:16:57 -0700

    client request test

--- client_test.go
+++ client_test.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/onsi/gomega"
 	"reflect"
 	"net"
-	
+	"encoding/json"
 )
 
 var _ = Describe("Client", func() {
@@ -52,11 +52,36 @@ var _ = Describe("Client", func() {
 		}
 	})
 
-	It("can use request to write a capsule to a socket", func() {
-	
+	It("can use request to write a capsule to a socket", func() {	
+		listener, err := net.Listen("tcp", "localhost:5009")
+		Expect(err).To(BeNil())
+		defer listener.Close()
+		sockets := make(chan net.Conn, 1)
+		go func() {
+			conn, _ := listener.Accept()
+			sockets <- conn
+		}()
+		client_side, err := net.Dial("tcp", "localhost:5009")
+		Expect(err).To(BeNil())
+		defer client_side.Close()
+		server_side := <- sockets
+		client := NewClient(client_side, &populated_type_store)
+		_, err = client.Request(thingy)
+		Expect(err).To(BeNil())
+		iface, err := NextStruct(server_side, &populated_type_store)
+		if capsule, correct_type := iface.(*Capsule); correct_type {
+			Expect(capsule.Type).To(Equal(uint16(1)))
+			restored_thing := &Thingy{}
+			err = json.Unmarshal([]byte(capsule.Data), &restored_thing)
+			Expect(err).To(BeNil())
+			Expect(restored_thing.ID).To(Equal(thingy.ID))
+			Expect(restored_thing.Name).To(Equal(thingy.Name))
+		} else {
+			Expect(correct_type).To(Equal(true))
+		}
 	})
 
 	It("can run on response callbacks", func() {
-	
+		
 	})
 })
